7489652288da7d6353c051ccb7bb66ded3e7801f
HemantDimri27 <dimrihemant27@gmail.com>
2024-03-16 11:16:29 +0530

    png

--- src/components/Projects/projectList.js
+++ src/components/Projects/projectList.js
@@ -1,24 +1,57 @@
+import img1 from '../../assets/mega-blog.png'
+import img2 from '../../assets/todoApp.png'
+import img3 from '../../assets/currency-Converter.png'
+import img4 from '../../assets/weather.png'
+import img5 from '../../assets/youtube-clone.png'
+import img6 from '../../assets/barber-shop.png'
 
-import img2 from '../../assets/weather.png'
-import img3 from '../../assets/todoApp.png'
+
 
 
 const ProjectsList = [
     {
+        id: 1,
+        title: 'Mega-Blog',
+        link: '',
+        github: 'https://github.com/HemantDimri27/MegaBlog',
+        image: img1
+    },
+    {
         id: 2,
-        title: 'Current Weather WebApp',
-        link: 'https://weather-webapp-pro.vercel.app',
-        github: 'https://github.com/chiragjain307/Weather-WebApp',
+        title: 'todo',
+        link: 'https://todo0027.netlify.app/',
+        github: 'https://github.com/HemantDimri27/Todo',
         image: img2
     },
     {
         id: 3,
-        title: 'Todo WebApp',
-        link: 'https://todo-webapp-pro.vercel.app',
-        github: 'https://github.com/chiragjain307/Todo-WebApp',
+        title: 'Currency-Converter',
+        link: 'https://currency-convertor0027.netlify.app/',
+        github: 'https://github.com/HemantDimri27/Currency-Convertor',
         image: img3
     },
-        
+    {
+        id: 4,
+        title: 'Weather-WebApp',
+        link: 'https://weather-web-app0027.netlify.app/',
+        github: 'https://github.com/HemantDimri27/Weather-WebApp',
+        image: img4
+    },
+    {
+        id: 5,
+        title: 'Youtube-Clone',
+        link: 'https://youtube0027.netlify.app/',
+        github: 'https://github.com/HemantDimri27/YouTube-Clone',
+        image: img5
+    },
+    
+    {
+        id: 6,
+        title: 'Barber-Shop',
+        link: 'https://barber-sphop0027.netlify.app/',
+        github: 'https://github.com/HemantDimri27/Barber_Shop',
+        image: img6
+    }, 
     
 ]
 
